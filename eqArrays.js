844de54b0f8fe5a816c068db623eb5bcbe85272d
Alexandra Verenceanu <alexandra.verenceanu@mail.mcgill.ca>
2021-11-24 04:15:04 +0000

    eqArrats file added

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed ✅: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`‼️ Assertion Failed ‼️ : ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return (false);
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+
+    }
+  }
+  return true;
+};
+
+
+//eqArrays(["1", "2", "3"], ["1", "2", 3])
+
+assertEqual(eqArrays(["1", "2", "3", "4"], ["1", "2", "3"]), false);
