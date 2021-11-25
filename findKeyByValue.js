c94e7a23516fae6cdb7b7ad85316c48eda9f8331
Alexandra Verenceanu <alexandra.verenceanu@mail.mcgill.ca>
2021-11-25 02:22:31 +0000

    findKeyByValue function added

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed ✅: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`‼️ Assertion Failed ‼️ : ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKeyByValue = function (listToLook, elementToFind) {
+  for (element in listToLook) {
+    if (listToLook[element] === elementToFind) {
+      return element;
+    } 
+    else if (listToLook[element] === elementToFind) {
+      return undefined;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
