b31417e55a569cf45a6b63fe7f9198fe453d7245
Alexandra Verenceanu <alexandra.verenceanu@mail.mcgill.ca>
2021-11-29 19:49:34 +0000

    Mocha and Chai added to middleTest file

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,16 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual(middle([1,2,3,4]),[2,3]);
+describe('middle function', () => {
+  it('should return [2, 3] when given [1,2,3,4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3])
+  });
+
+  it('should return [2] when given [1,2,3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2])
+  });
+
+  it('should return [] when given [1,2]', () => {
+    assert.deepEqual(middle([1, 2]), [])
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,8 @@
 const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
 describe('tail function', () => {
-  it('should return [fegege, gege] when given "hebi", "fegege", "gege"]', () => {
+  it('should return [fegege, gege] when given ["hebi", "fegege", "gege"]', () => {
     assert.deepEqual(tail(["hebi", "fegege", "gege"]), ["fegege", "gege"])
   });
 });
