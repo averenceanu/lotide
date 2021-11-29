622dc85d8150c34103932ee74891f43dbd2adbd7
Alexandra Verenceanu <alexandra.verenceanu@mail.mcgill.ca>
2021-11-29 19:06:00 +0000

    eqArrays function refractored and test file added

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed ✅: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`‼️ Assertion Failed ‼️ : ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return (false);
@@ -20,7 +12,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-
-//eqArrays(["1", "2", "3"], ["1", "2", 3])
-
-assertEqual(eqArrays(["1", "2", "3", "4"], ["1", "2", "3"]), false);
+module.exports = eqArrays; 
