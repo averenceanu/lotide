833ece567d25eb139773cf755f99e1f000af77aa
Alexandra Verenceanu <alexandra.verenceanu@mail.mcgill.ca>
2021-11-29 18:49:02 +0000

    Refractored + created test files for assertEqual and head function

--- head.js
+++ head.js
@@ -1,15 +1,6 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed ✅: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`‼️ Assertion Failed ‼️ : ${actual} !== ${expected}`);
-  }
-};
-
 const head = function (element) {
   let firstElement = element[0];
   return firstElement;
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head; 
